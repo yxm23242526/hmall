4b23a41e16c47b1b17eb6c9d906a66a690bc56c9
杨肖铭 <651260437@qq.com>
2024-07-24 16:49:18 +0800

    2024.7.24 openfeign结束

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,13 +1,17 @@
 package com.hmall.cart;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.spring.web.plugins.DefaultConfiguration;
 
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
